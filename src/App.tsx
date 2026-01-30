e171bf8d36481d5496c43ff7a73564c2df7f2967
oscar <oscar@familify.com>
2026-01-30 13:44:03 -0500

    feat: mega-compact vertical fit and scroll-to-top

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
+import ScrollToTop from './components/common/ScrollToTop';
 import GlobalImagePreloader from './components/common/GlobalImagePreloader';
 import './styles/global.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <GlobalImagePreloader />
       <AppRoutes />
     </BrowserRouter>
