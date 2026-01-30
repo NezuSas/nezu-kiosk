d26254e566c3db035ea5c6b6eede2b89b0a4f6fc
oscar <oscar@familify.com>
2026-01-30 12:18:57 -0500

    fix: resolve typescript unused variables to fix build

--- src/components/layout/MainLayout/MainLayout.tsx
+++ src/components/layout/MainLayout/MainLayout.tsx
@@ -1,41 +1,14 @@
 import { useEffect, useState, useRef } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Footer } from "@/components/common/Footer";
 import styles from "./MainLayout.module.css";
 import { type MainLayoutProps } from "./MainLayout.types";
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
   const location = useLocation();
-  const navigate = useNavigate();
   const isHome = location.pathname === "/";
   const [isIdle, setIsIdle] = useState(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  let idleTimer: NodeJS.Timeout;
-
-  // useEffect(() => {
-  //   const resetIdleTimer = () => {
-  //     clearTimeout(idleTimer);
-  //     setIsIdle(false);
-  //     idleTimer = setTimeout(() => {
-  //       setIsIdle(true);
-  //     }, 300000);
-  //   };
-
-  //   window.addEventListener("mousemove", resetIdleTimer);
-  //   window.addEventListener("keydown", resetIdleTimer);
-  //   window.addEventListener("touchstart", resetIdleTimer);
-  //   window.addEventListener("click", resetIdleTimer);
-
-  //   resetIdleTimer();
-
-  //   return () => {
-  //     clearTimeout(idleTimer);
-  //     window.removeEventListener("mousemove", resetIdleTimer);
-  //     window.removeEventListener("keydown", resetIdleTimer);
-  //     window.removeEventListener("touchstart", resetIdleTimer);
-  //     window.removeEventListener("click", resetIdleTimer);
-  //   };
-  // }, [navigate]);
 
   useEffect(() => {
     if (isIdle && videoRef.current) {
